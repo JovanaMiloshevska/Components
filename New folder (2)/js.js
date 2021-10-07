73d09e879c43e9ffd6828635fd6fb3a387ee404e
JovanaMiloshevska <90845753+JovanaMiloshevska@users.noreply.github.com>
2021-10-07 09:50:54 +0200

    Add files via upload

--- New folder (2)/js.js
+++ New folder (2)/js.js
@@ -1,23 +1,23 @@
 debugger;
 let buttonAction = document.getElementById('btnaction');
 let photos = document.getElementsByClassName("box");
-for (let i=4; i < photos.length; i++)
+for (let i=photos.length/2; i < photos.length; i++)
 photos[i].style.display = "none";
 
     buttonAction.addEventListener('click', function(){
             if (photos[4].style.display === "none" && photos[5].style.display === "none" && photos[6].style.display === "none" && photos[7].style.display === "none"){
-                for (let i = 0; i < 4; i++){
+                for (let i = 0; i < photos.length/2; i++){
                     photos[i].style.display = "none";
                 }
-                for (let i = 4; i < 8; i++){
+                for (let i = photos.length/2; i < photos.length; i++){
                     photos[i].style.display = "flex";
                 }
             }
             else{
-                for (let i = 0; i < 4; i++){
+                for (let i = 0; i < photos.length/2; i++){
                     photos[i].style.display = "flex";
                 }
-                for (let i = 4; i < 8; i++){
+                for (let i = photos.length/2; i < photos.length; i++){
                     photos[i].style.display = "none";
                 }
             }
