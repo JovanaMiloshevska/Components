1f374c4be5741f0960e2230b79e7b92cc8486cc0
JovanaMiloshevska <90845753+JovanaMiloshevska@users.noreply.github.com>
2021-10-07 12:30:10 +0200

    Add files via upload

--- New folder (2)/js.js
+++ New folder (2)/js.js
@@ -1,10 +1,29 @@
 debugger;
-let buttonAction = document.getElementById('btnaction');
+let buttonPrevious = document.getElementById('btn-prev');
+let buttonNext = document.getElementById('btn-next')
 let photos = document.getElementsByClassName("box");
 for (let i=photos.length/2; i < photos.length; i++)
 photos[i].style.display = "none";
 
-    buttonAction.addEventListener('click', function(){
+    buttonPrevious.addEventListener('click', function(){
+            if (photos[0].style.display === "none" && photos[1].style.display === "none" && photos[2].style.display === "none" && photos[3].style.display === "none"){
+                for (let i = 0; i < photos.length/2; i++){
+                    photos[i].style.display = "flex";
+                }
+                for (let i = photos.length/2; i < photos.length; i++){
+                    photos[i].style.display = "none";
+                }
+            }
+            else {
+                for (let i = 0; i < photos.length/2; i++){
+                    photos[i].style.display = "flex";
+                }
+            }
+            
+        
+        
+         })
+         buttonNext.addEventListener('click', function(){
             if (photos[4].style.display === "none" && photos[5].style.display === "none" && photos[6].style.display === "none" && photos[7].style.display === "none"){
                 for (let i = 0; i < photos.length/2; i++){
                     photos[i].style.display = "none";
@@ -14,13 +33,13 @@ photos[i].style.display = "none";
                 }
             }
             else{
-                for (let i = 0; i < photos.length/2; i++){
+                for (let i = photos.length/2; i < photos.length; i++){
                     photos[i].style.display = "flex";
                 }
-                for (let i = photos.length/2; i < photos.length; i++){
-                    photos[i].style.display = "none";
-                }
             }
+                
         
         
          })
+
+         
