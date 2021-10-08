2f20f0324e2b0acfc393128f4a3d09d28eff4e53
JovanaMiloshevska <90845753+JovanaMiloshevska@users.noreply.github.com>
2021-10-08 10:40:23 +0200

    Add files via upload

--- New folder (2)/js.js
+++ New folder (2)/js.js
@@ -1,45 +1,47 @@
-debugger;
 let buttonPrevious = document.getElementById('btn-prev');
 let buttonNext = document.getElementById('btn-next')
 let photos = document.getElementsByClassName("box");
-for (let i=photos.length/2; i < photos.length; i++)
-photos[i].style.display = "none";
+let nextIndex = 4;
 
-    buttonPrevious.addEventListener('click', function(){
-            if (photos[0].style.display === "none" && photos[1].style.display === "none" && photos[2].style.display === "none" && photos[3].style.display === "none"){
-                for (let i = 0; i < photos.length/2; i++){
-                    photos[i].style.display = "flex";
-                }
-                for (let i = photos.length/2; i < photos.length; i++){
-                    photos[i].style.display = "none";
-                }
-            }
-            else {
-                for (let i = 0; i < photos.length/2; i++){
-                    photos[i].style.display = "flex";
-                }
-            }
-            
-        
-        
-         })
-         buttonNext.addEventListener('click', function(){
-            if (photos[4].style.display === "none" && photos[5].style.display === "none" && photos[6].style.display === "none" && photos[7].style.display === "none"){
-                for (let i = 0; i < photos.length/2; i++){
-                    photos[i].style.display = "none";
-                }
-                for (let i = photos.length/2; i < photos.length; i++){
-                    photos[i].style.display = "flex";
-                }
-            }
-            else{
-                for (let i = photos.length/2; i < photos.length; i++){
-                    photos[i].style.display = "flex";
-                }
-            }
-                
-        
-        
-         })
+window.addEventListener('DOMContentLoaded', () => {
+    for (let i = 4; i < photos.length; i++) {
+        photos[i].style.display = "none";
+    }
 
-         
+    document.cookie = `nextIndex: ${nextIndex}`;
+});
+
+const parseIndexFromCookie = (cookieString) => parseInt(cookieString.split(":")[1]);
+
+buttonNext.addEventListener('click', function() {
+    let currentIndex = parseIndexFromCookie(document.cookie);
+    let previousIndex = currentIndex - 4;
+    let nextIndex = currentIndex + 4;
+
+    for(let index = currentIndex; index < nextIndex; ++index) {
+        photos[index].style.display = "flex"
+    }
+    
+    for(let index = previousIndex; index < currentIndex; ++index) {
+        photos[index].style.display = "none"
+    }
+
+    document.cookie = `nextIndex: ${nextIndex}`;
+}) 
+
+buttonPrevious.addEventListener('click', function() {
+    let currentIndex = parseIndexFromCookie(document.cookie);
+    let previousIndex = currentIndex - 8;
+    let nextIndex = previousIndex + 4;
+
+    for(let index = previousIndex; index < nextIndex; ++index) {
+        photos[index].style.display = "flex"
+    }
+
+    for(let index = nextIndex; index < currentIndex; ++index) {
+        photos[index].style.display = "none"
+    }
+
+    document.cookie = `nextIndex: ${nextIndex}`;
+
+})
